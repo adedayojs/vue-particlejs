fea1dd5c2b7f5d00e5e4a80f0a750befe97c9361
adedayojs <samfeolu@gmail.com>
2019-10-23 22:25:30 +0100

    Deletion: Removed Some Unnecessary codes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app"></div>
+<div></div>
 </template>
 
 <script>
@@ -23,21 +23,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-/* #particles-js {
-  width: 100%;
-  height: 100%;
-  background-color: #b61924;
-  background-image: url("");
-  background-size: cover;
-  background-position: 50% 50%;
-  background-repeat: no-repeat;
-} */
 </style>
