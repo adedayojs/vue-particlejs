9783c7007b84ef3619d0b526fc286206e8d4ad8f
adedayojs <samfeolu@gmail.com>
2019-10-24 01:57:44 +0100

    Completion: Restructured Whole App, Moved to a whole new approach and moving to version 2

---
+++ src/components/VueParticle.vue
@@ -0,0 +1,31 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import "particles.js";
+
+export default {
+  name: "VueParticleJs",
+
+  props: {
+    config: Object,
+    domId: String
+  },
+  mounted() {
+
+    if (typeof this.domId !== "string") throw new Error("Invalid Dom object Provided");
+    window.particlesJS(this.domId, {
+      particles:this.config.particles,
+      interactivity:this.config.interactivity,
+      retina_detect:this.config.retina_detect
+    }, function() {
+      // eslint-disable-next-line no-console
+      console.log("callback - particles.js config loaded");
+    });
+  }
+};
+</script>
+
+<style>
+</style>
